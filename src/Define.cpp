8328c4f34c19a6d9a969e575ffec586879aedc18
tyobi-dev <tyobi.dev@gmail.com>
2024-11-17 21:35:04 +0900

    first commit

---
+++ src/Define.cpp
@@ -0,0 +1,8 @@
+﻿#include "Define.h"
+
+const String Define::DATA_DIR =
+#ifdef WEB_APP
+U"example/";
+#else
+U"../example/";
+#endif
---
+++ src/Define.h
@@ -0,0 +1,25 @@
+﻿# pragma once
+//# define NO_S3D_USING
+# include <Siv3D.hpp>
+
+//Webビルドするときにフラグを立てる。
+//#define WEB_APP 1
+
+
+class Define final {
+
+public:
+	//=============　定数 =================
+	enum
+	{
+		BLOCK_GROUND_TOP_NUM = 5,
+		//BLOCK_GROUND_BOTTOM_NUM = 3,
+		BLOCK_GROUND_BOTTOM_NUM = 5,
+
+	};
+
+
+	//リソースのディレクトリ
+	const static String DATA_DIR;
+
+};
